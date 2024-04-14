cd10fcea1aeeb5021054dcf3db9a55ae0d3594bd
fprh13 <fprh13@gmail.com>
2024-04-14 21:01:17 +0900

    - Project Metadata 정의 - 초기 Dependencies 선정

---
+++ src/test/java/com/carumuch/capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carumuch.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

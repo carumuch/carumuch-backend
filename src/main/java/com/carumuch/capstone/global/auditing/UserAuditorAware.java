3560834c6ec17ce0a7208bf40763df7f4fad6b42
fprh13 <fprh13@gmail.com>
2024-05-13 00:46:04 +0900

    #17 Feat: AuditorAware 추가

---
+++ src/main/java/com/carumuch/capstone/global/auditing/UserAuditorAware.java
@@ -0,0 +1,25 @@
+package com.carumuch.capstone.global.auditing;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserAuditorAware implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            return Optional.empty();
+        }
+        return Optional.of(authentication.getName());
+    }
+}
+
+
+
+
+

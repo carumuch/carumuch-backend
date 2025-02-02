e271e13d01b0a769940f59381a1454814b846aea
조영무 <fprh13@gmail.com>
2025-02-02 11:27:25 +0900

    #68 chore: RestDocsSupport 구성 수정

--- src/test/java/com/module/RestDocsSupport.java
+++ src/test/java/com/module/RestDocsSupport.java
@@ -2,38 +2,40 @@ package com.module;
 
 import com.carumuch.capstone.domain.auth.controller.AuthController;
 import com.carumuch.capstone.domain.auth.jwt.TokenProvider;
-import com.carumuch.capstone.domain.bid.controller.BidController;
-import com.carumuch.capstone.domain.board.controller.BoardController;
-import com.carumuch.capstone.domain.bodyshop.controller.BodyShopController;
-import com.carumuch.capstone.domain.estimate.controller.EstimateController;
-import com.carumuch.capstone.domain.image.controller.ImageController;
-import com.carumuch.capstone.domain.user.controller.UserController;
-import com.carumuch.capstone.domain.vehicle.controller.VehicleController;
+import com.carumuch.capstone.domain.auth.service.AuthService;
+import com.carumuch.capstone.domain.auth.service.CustomOAuth2UserService;
+import com.carumuch.capstone.global.config.SecurityConfig;
+import com.carumuch.capstone.support.config.RestDocsConfig;
+import com.carumuch.capstone.support.config.TestAuditorAwareConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @WebMvcTest(controllers = {
-        AuthController.class,
-        UserController.class,
-        ImageController.class,
-        BoardController.class,
-        VehicleController.class,
-        EstimateController.class,
-        BidController.class,
-        BodyShopController.class
+        AuthController.class
+//        UserController.class,
+//        ImageController.class,
+//        BoardController.class,
+//        VehicleController.class,
+//        EstimateController.class,
+//        BidController.class,
+//        BodyShopController.class
 })
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import({SecurityConfig.class,TestAuditorAwareConfig.class, RestDocsConfig.class})
 public abstract class RestDocsSupport {
+
+    @Autowired
+    protected RestDocumentationResultHandler restDocsHandler;
 
     @Autowired
     protected MockMvc mockMvc;
@@ -44,12 +46,12 @@ public abstract class RestDocsSupport {
     @MockBean
     protected TokenProvider tokenProvider;
 
-    @BeforeEach
-    void setUpMockMvcForRestDocs(WebApplicationContext webApplicationContext,
-                                 RestDocumentationContextProvider restDocumentationContextProvider) {
-        this.mockMvc = MockMvcBuilders
-                .webAppContextSetup(webApplicationContext)
-                .apply(MockMvcRestDocumentation.documentationConfiguration(restDocumentationContextProvider))
-                .build();
-    }
+    @MockBean
+    protected CustomOAuth2UserService customOAuth2UserService;
+
+    @MockBean
+    protected BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @MockBean
+    protected AuthService authService;
 }

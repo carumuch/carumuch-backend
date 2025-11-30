ddeae3b78a0d6663e76d61eee191f4714184216e
조영무 <fprh13@gmail.com>
2025-12-01 04:48:09 +0900

    chore: S3 config common으로 이동 #72

--- src/main/java/com/carumuch/capstone/common/infrastructure/config/S3Config.java
+++ src/main/java/com/carumuch/capstone/common/infrastructure/config/S3Config.java
@@ -1,4 +1,4 @@
-package com.carumuch.capstone.common.legacy.config;
+package com.carumuch.capstone.common.infrastructure.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;

cb9b910d4b1f5687c9752cc216f005cc9edf4100
조영무 <fprh13@gmail.com>
2026-01-04 23:38:40 +0900

    chore: DLQ 저장 실패 Counter 메트릭 추가 및 추적을 위한 로그 정보 추가 #88

--- src/main/java/com/carumuch/capstone/estimate/infrastructure/mq/dlq/EstimateResultDlqListener.java
+++ src/main/java/com/carumuch/capstone/estimate/infrastructure/mq/dlq/EstimateResultDlqListener.java
@@ -8,6 +8,9 @@ import com.carumuch.capstone.estimate.infrastructure.mq.config.RabbitMqConfig;
 import com.carumuch.capstone.estimate.infrastructure.mq.message.EstimateResultMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,6 +21,18 @@ public class EstimateResultDlqListener {
 
 	private final DlqMessageRepository dlqMessageRepository;
 	private final ObjectMapper objectMapper;
+	private final MeterRegistry meterRegistry;
+
+	private Counter dlqFailCounter;
+
+	@PostConstruct
+	void initDlqFailCounter() {
+		this.dlqFailCounter = Counter.builder("estimate.dlq.save.fail.total")
+			.description("Estimate DLQ save fail")
+			.tag("class", this.getClass().getName())
+			.tag("queue", RabbitMqConfig.ESTIMATE_RESULT_DLQ)
+			.register(meterRegistry);
+	}
 
 	@Transactional
 	@RabbitListener(queues = RabbitMqConfig.ESTIMATE_RESULT_DLQ)
@@ -29,7 +44,8 @@ public class EstimateResultDlqListener {
 			);
 
 		} catch (Exception e) {
-			log.error("DLQ 메세지 저장 실패: message={}", message.toString());
+			log.error("DLQ 메세지 저장 실패: message={}", message, e);
+			dlqFailCounter.increment();
 		}
 	}
 }
